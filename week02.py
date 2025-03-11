9b9985dd34925eba57e791dab44c7636bff005c0
sothdud <sy031121@naver.com>
2025-03-11 20:22:34 +0900

    week02 kiosk v0.4

--- week02.py
+++ week02.py
@@ -1,13 +1,19 @@
 # 1) Ice Americano : 2000 2) Cafe latte : 3000
-menu = input("1) Ice Americano 2) Cafe Latte 3) Exit :")
-if menu=="1":
-    print("Ice Americano ordered. Price : 2000 won")
+prices=[2000,3000]
+drinks=["Ice Americano","Cafe Latte"]
+while True:
 
-elif menu=="2":
-    print("Cafe latte ordered. Price : 3000 won")
+    menu = input(f"1) {drinks[0]} {prices[0]}won 2) {drinks[1]}{prices[1]} 3) Exit :")
+    if menu=="1":
+        print(f"Ice Americano ordered. Price :{prices[0]} won")
 
-elif menu=="3":
-    print("Finish order~")
-    break
+    elif menu=="2":
+        print(f"Cafe latte ordered. Price : {prices[1]}won")
+
+    elif menu=="3":
+        print("Finish order~")
+        break
 
 
+    else:
+        print(f"{menu} menu is not exist.please choose from above menu")
